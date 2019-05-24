332c2a710cf29f2c13c6e89f28bda3ea5fc89ebe
student <my@yandex.ru>
2019-05-24 13:26:28 +0300

    add ex

---
+++ sources/examples/skip_lines.pas
@@ -0,0 +1,34 @@
+﻿
+var
+ f_in, f_out: Text;
+ i, n: integer; 
+ s1, s2: string;
+begin
+ n := 5;
+ 
+ i := 0;
+ 
+ assign(f_in, 'listtst.txt');
+ reset(f_in);
+ assign(f_out, 'listtst.~xt');
+ rewrite(f_out);
+ 
+ while not eof(f_in) do
+ begin
+   i := i + 1;
+   readln(f_in, s1);   
+   if eof(f_in) then break;
+   readln(f_in, s2);
+   
+   if i <> n then
+   begin
+     writeln(f_out, s1);   
+     writeln(f_out, s2);    
+   end;   
+ end;
+ 
+ close(f_in);
+ close(f_out); 
+ erase(f_in);
+ rename(f_out, 'listtst.txt');
+end.
---
+++ sources/examples/skip_lines2.pas
@@ -0,0 +1,47 @@
+﻿type
+ TTest = record
+   Code, FileName: string;
+ end;
+
+var
+ f_in: Text;
+ 
+ arr: array of TTest;
+ 
+ i, n: integer; 
+ 
+ s1, s2: string;
+begin
+ n := 5;
+ 
+ i := 0;
+ 
+ assign(f_in, 'listtst.txt');
+ reset(f_in);
+ 
+ while not eof(f_in) do
+ begin
+   i := i + 1;
+   
+   SetLength(arr, i);
+   
+   readln(f_in, arr[i].Code);  
+   
+   if eof(f_in) then break;
+   
+   readln(f_in, arr[i].FileName);
+ end;
+ 
+ close(f_in);
+ 
+ rewrite(f_in);
+ 
+ for i := low(arr) to high(arr) do 
+   if i <> n then
+   begin
+     writeln(f_in, arr[i].Code);   
+     writeln(f_in, arr[i].FileName);    
+   end;    
+
+ close(f_in);  
+end.
